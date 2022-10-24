db8b8b62a798928773291595c3775728e69dd1f1
samsonOdunola <odunolasamson@yahoo.com>
2022-10-24 16:15:51 +0100

    major upgrades

--- src/App.js
+++ src/App.js
@@ -1,17 +1,102 @@
-import socketClient from "socket.io-client";
-import { useRef, useEffect, useState } from "react";
-
+// import socketClient from "socket.io-client";
+import io from "socket.io-client";
+import { v4 } from "uuid";
+import { useEffect, useState } from "react";
 import Chat from "./components/chat";
+const socket = io(process.env.REACT_APP_ENDPOINT);
 function App() {
-  let socket = useRef();
-  let endpoint = process.env.REACT_APP_ENDPOINT;
-  const [loading, setLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [newMessage, setNewMessage] = useState("");
+  const [user, setUser] = useState("");
+  const [room, setRoom] = useState("");
+  const [chatIsVisisble, setChatIsVisible] = useState(false);
+  const [messages, setMessages] = useState([]);
+
+  // let socket = useRef();
+  // let endpoint = process.env.REACT_APP_LOCALENDPOINT;
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    socket.current = socketClient(endpoint);
-    setLoading(true);
-  }, []);
-  return <>{loading && <Chat socket={socket} />}</>;
+    console.log("connected", socket.connected);
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
+    // socket.current = socketClient(endpoint);
+    // setLoading(true);
+  }, [isConnected]);
+  useEffect(() => {
+    socket.on("broadcast_msg", ({ user, message }) => {
+      console.log(user, message);
+      console.log("socket working");
+      const msg = `${user} send:${message}`;
+
+      setMessages((prevState) => [msg, ...prevState]);
+      console.log(messages);
+    });
+  }, [socket]);
+  const handleEnterChatRoom = () => {
+    if (user !== "" && room !== "") {
+      setChatIsVisible(true);
+      socket.emit("join_room", { user, room });
+    }
+  };
+
+  const handleSendMessage = () => {
+    const newMsgData = { room: room, user: user, message: newMessage };
+
+    socket.emit("send_msg", newMsgData);
+    const msg = `${user} send:${newMessage}`;
+    setNewMessage((prevState) => [msg, ...prevState]);
+    setNewMessage("");
+  };
+
+  return (
+    <>
+      {chatIsVisisble ? (
+        <Chat
+          room={room}
+          user={user}
+          handleSendMessage={handleSendMessage}
+          newMessage={newMessage}
+          setNewMessage={setNewMessage}
+          messages={messages}
+          v4={v4}
+        />
+      ) : (
+        <div className="enter-details">
+          <h1>
+            Input your name and chat room you wish to join, you can also create
+            a new room{" "}
+          </h1>
+          <input
+            type="text"
+            placeholder="Enter name"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="room"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />{" "}
+          <br></br>
+          <button onClick={() => handleEnterChatRoom()}> Enter</button>
+          <p>
+            Designed and coded by <strong>Samson Odunola</strong>{" "}
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/chat.js
+++ src/components/chat.js
@@ -1,56 +1,96 @@
 import { FiSend } from "react-icons/fi";
-import { useState, useEffect } from "react";
-const Chat = ({ socket }) => {
-  const [message, setMessage] = useState("");
-  const [allMessage, setAllMessage] = useState([]);
+
+const Chat = ({
+  room,
+  user,
+  handleSendMessage,
+  newMessage,
+  setNewMessage,
+  messages,
+  v4,
+}) => {
+  // const [message, setMessage] = useState("");
+  // const [allMessage, setAllMessage] = useState([]);
   // const [currentId, setCurrentId] = useState("");
-  const [postMessage, setPostMessage] = useState("");
+  // const [postMessage, setPostMessage] = useState("");
   // socket.current.on("user_id", (userid) => {
   //   setCurrentId(userid);
   //   console.log(userid);
   // });
-  const sendMessage = () => {
-    setPostMessage(message);
-    socket.current.emit("sendMsg", message);
-  };
-  useEffect(() => {
-    socket.current.on("broadCastMsg", (recivedMessage) => {
-      let placeHolder = [...allMessage];
-      placeHolder.push(recivedMessage);
-
-      setAllMessage(placeHolder);
-    });
-  }, [postMessage]);
+  // const sendMessage = () => {
+  //   // setPostMessage(message);
+  //   socket.current.emit("sendMsg", message);
+  // };
+  // useEffect(() => {
+  //   if (socket.current) {
+  //     socket.current.on("broadCastMsg", (recivedMessage) => {
+  //       console.log(recivedMessage);
+  //       console.log(allMessage);
+  //       let placeHolder = [...allMessage, recivedMessage];
+  //       setAllMessage(placeHolder);
+  //     });
+  //   }
+  // }, [message]);
 
   return (
     <div className="App">
       <div className="header">
-        <h1>Chat Room</h1>
+        <h1>
+          Room:{room} | User:{user}
+        </h1>
       </div>
       <div className="chatbody">
-        {allMessage.map((message, index) => {
+        {messages.map((el, index) => {
+          // return (
+          //   <p className="chatmsg" key={index}>
+          //     {el}
+          //   </p>
+          // );
+          if (el.split(" ")[0] === user) {
+            return (
+              <p
+                className="chatmsg"
+                style={{
+                  alignSelf: "flex-start",
+                  backgroundColor: "rgb(24, 24, 24)",
+                }}
+                key={v4()}
+              >
+                {el.split(":")[1]}
+              </p>
+            );
+          } else {
+            return (
+              <p
+                className="chatmsg"
+                style={{
+                  alignSelf: "flex-end",
+                  backgroundColor: "rgb(34, 205, 77)",
+                }}
+                key={v4()}
+              >
+                {el.split(":")[1]}
+              </p>
+            );
+          }
+        })}
+        {/* {messages.map((message, index) => {
           return (
             <p className="chatmsg" key={index}>
               {message}
             </p>
           );
-        })}
+        })} */}
       </div>
       <div className="input">
         <div>
           <input
-            value={message}
+            value={newMessage}
             type="text"
             placeholder="Type a message"
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={(e) => setNewMessage(e.target.value)}
           />{" "}
-          <FiSend
-            className="button"
-            onClick={() => {
-              sendMessage();
-              setMessage("");
-            }}
-          />
+          <FiSend className="button" onClick={() => handleSendMessage()} />
         </div>
       </div>
     </div>
